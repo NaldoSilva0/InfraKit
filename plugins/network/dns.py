bc1203e2e09bb2ba3a55d02085438bd315176590
Naldo <naldo20silva20@gmail.com>
2026-08-26 11:38:42 -0300

    Better dns result

--- plugins/network/dns.py
+++ plugins/network/dns.py
@@ -1,4 +1,5 @@
-import socket
+
+import dns.resolver
 from core.plugin import Plugin
 from core.result import PluginResult
 
@@ -6,27 +7,115 @@ class DNSPlugin(Plugin):
     def __init__(self):
         super().__init__("DNS", "Obtém informações DNS do alvo")
 
+    def consultar_dns(self, target):
+        try:
+            ipv4 = dns.resolver.resolve(target, "A")
+        except dns.resolver.NoAnswer:
+            ipv4 = []
+
+        try:    
+            ipv6 = dns.resolver.resolve(target, "AAAA")
+        except dns.resolver.NoAnswer:
+            ipv6 = []
+        except dns.resolver.NXDOMAIN:
+            raise
+        except dns.resolver.Timeout:
+            raise
+
+        try:    
+            mx = dns.resolver.resolve(target, "MX")
+        except dns.resolver.NoAnswer:
+            mx = []
+        except dns.resolver.NXDOMAIN:
+            raise
+        except dns.resolver.Timeout:
+            raise
+
+        try:
+            ns = dns.resolver.resolve(target, "NS")
+        except dns.resolver.NoAnswer:
+            ns = []
+        except dns.resolver.NXDOMAIN:
+            raise
+        except dns.resolver.Timeout:
+            raise
+
+        try:
+            txt = dns.resolver.resolve(target, "TXT")
+        except dns.resolver.NoAnswer:
+            txt = []
+        except dns.resolver.NXDOMAIN:
+            raise
+        except dns.resolver.Timeout:
+            raise
+
+        try:
+            cname = dns.resolver.resolve(target, "CNAME")
+        except dns.resolver.NoAnswer:
+            cname = []
+        except dns.resolver.NXDOMAIN:
+            raise
+        except dns.resolver.Timeout:
+            raise
+        
+        retornar_listas = (ipv4, ipv6, mx, ns, txt, cname)
+        return retornar_listas
+
+
+
+
     def run(self, target):
+
+
         if target.startswith("http://"):
             target = target.replace("http://", '')
 
         elif target.startswith("https://"):
            target = target.replace("https://", '') 
-
+        
         else:
             pass
 
         try:
-            resultado = socket.gethostbyname(target)
-            status = "SUCESSO!"
-            retornar = PluginResult(self.name, status, resultado)
-    
-            return retornar
-        except:
-            status = "ERRO!"
-            resultado = "Não foi possível resolver o domínio"
-            retornar = PluginResult(self.name, status, resultado)
+            ipv4, ipv6, mx, ns, txt, cname = self.consultar_dns(target)
+
+        except dns.resolver.NXDOMAIN:
+            return PluginResult(self.name, "ERRO!", "O domínio não existe")
+        except dns.resolver.Timeout:
+           return PluginResult(self.name, "ERRO!","Não obtivemos uma resposta DNS dentro do tempo esperado")
+            
+
+        status = "SUCESSO!"   
+
+        ipv4 = "\n".join(str(ip4) for ip4 in ipv4)
+        ipv6 = "\n".join(str(ip6) for ip6 in ipv6)
+        mx = "\n".join(str(m) for m in mx)
+        ns = "\n".join(str(n) for n in ns)
+        txt = "\n".join(str(tx) for tx in txt)
+        cname = "\n".join(str(cna) for cna in cname)
+
+        if not ipv4:
+            ipv4 = "Nenhum registro encontrado"
+
+        if not ipv6:
+            ipv6 = "Nenhum registro encontrado"
+
+        if not mx:
+            mx = "Nenhum registro encontrado"
+
+        if not ns:
+            ns = "Nenhum registro encontrado"
+
+        if not txt:
+            txt = "Nenhum registro encontrado"
+
+        if not cname:
+            cname = "Nenhum registro encontrado"
 
             
-            return retornar
+        resultado = f"A:\n{ipv4}\n\nAAAA:\n{ipv6}\n\nMX:\n{mx}\n\nNS:\n{ns}\n\nTXT:\n{txt}\n\nCNAME:\n{cname}"
+
+        retornar = PluginResult(self.name, status, resultado)    
+        return retornar
+
 
--- plugins/network/http.py
+++ plugins/network/http.py
@@ -15,16 +15,20 @@ class HttpPlugin(Plugin):
 
         try:
             resposta = requests.get(target)
-            if resposta.status_code == 200:
-                
+            codigo = resposta.status_code
+            servidor = resposta.headers.get("server")
+            tipo = resposta.headers.get("Content-Type")
+            hsts = resposta.headers.get("Strict-Transport-Security", "Não encontrado")
+
+            if codigo:
                 status = "SUCESSO!"
-                resultado = "Requisição realizada com sucesso! código HTTP: 200"
+                resultado = f"HTTP: {codigo}\nServer: {servidor}\nContent-Type: {tipo}\nHSTS: {hsts}"
                 retornar = PluginResult(self.name, status, resultado)
                 return retornar
             
-            elif resposta.status_code == 404:
+            else:
                 status = "ERRO!"
-                resultado = "Recurso não encontrado! Código HTTP: 404"
+                resultado = "Erro ao realizar a conexão/requisição"
                 retornar = PluginResult(self.name, status, resultado)
                 return retornar
         except:
